2c5455c29c7a80f21e035fcab993a326e594b119
dwj <2804345779@qq.com>
2020-08-09 15:19:16 +0800

    '删除多余文件'

--- vue-cars-admin-lesson23/README.md
+++
@@ -1,1 +0,0 @@
-# vue-cars-admin
--- vue-cars-admin-lesson23/src/views/amap/common.js
+++
@@ -1,7 +0,0 @@
-export function getLngLat(e){
-    return {
-        log: e.lnglat.getLng(),
-        lat: e.lnglat.getLat(),
-        value: e.lnglat.getLng() + ',' + e.lnglat.getLat()
-    }
-}
